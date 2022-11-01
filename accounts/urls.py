dca93cdd1877d9e8b4de0a013216d623f74d13a9
DongjaeKam <dongjaekam1994@gmail.com>
2022-11-01 15:48:15 +0900

    edit_profile 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-
     path('',views.login,name ='login'),
     path('logout/', views.logout, name='logout'),
-
-
+    path('update/',views.update,name='update'),
 ]
