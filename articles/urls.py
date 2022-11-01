f8b37682ab07bc52d22bf1b46b007e4e883fced7
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-11-01 15:53:22 +0900

    article-CRUD checkrequire

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = "articles"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("create/", views.create, name="review_create"),
+    path("<int:pk>/delete/", views.delete, name="review_delete"),
+    path("<int:pk>/detail/", views.detail, name="review_detail"),
+]
