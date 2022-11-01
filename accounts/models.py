8a60a1bf9f4beda7d0fa4140ec9d0839417264b3
DongjaeKam <dongjaekam1994@gmail.com>
2022-11-01 11:49:51 +0900

    초기설정

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    profile_name=models.CharField(max_length=100)
+    profile_image=models.ImageField(upload_to='/media/image/profile_image' , blank=True)
+    rank = models.IntegerField(default=0)
+
