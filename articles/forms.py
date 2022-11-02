f191ad56de706e1264c84a17ca7f7a0649d93f7d
devhum7p <devhum7p@gmail.com>
2022-11-02 09:50:53 +0900

    CRUD_update

--- articles/forms.py
+++ articles/forms.py
@@ -6,31 +6,38 @@ from django.forms import ClearableFileInput
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['title', 'content','place', 'theme']
+        fields = ["title", "content", "place", "theme"]
         labels = {
             "title": "제목",
             "content": "내용",
-            "place":"지역",
-            "theme":"테마",
+            "place": "지역",
+            "theme": "테마",
         }
+
+
 class PhotoForm(forms.ModelForm):
-  
     class Meta:
         model = Photo
-        fields = ('image', )
+        fields = ("image",)
         widgets = {
-            'image': ClearableFileInput(attrs={'multiple': True}),
+            "image": ClearableFileInput(attrs={"multiple": True}),
         }
+        labels = {
+            "image": "다중 클릭하여 사진을 여러장 올릴 수 있어요!",
+        }
+
+
 class CommentForm(forms.ModelForm):
-    
     class Meta:
         model = Comment
-        fields=['content',]
+        fields = [
+            "content",
+        ]
         labels = {
             "content": "",
         }
         error_messages = {
-            'content': {
-                'required':"",
+            "content": {
+                "required": "",
             },
         }
