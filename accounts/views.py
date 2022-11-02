5c7ae78a820c19dad52952d9c9df20d3f3a0c9ce
DongjaeKam <dongjaekam1994@gmail.com>
2022-11-02 11:17:17 +0900

    edit_profile, change_password  pk 추가 , 에러 처리용 잘못된 접근 페이지 추가

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
 from .forms import CustomUserModel
+from .forms import CustomUserChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.decorators import login_required
-from .forms import CustomUserChangeForm
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -26,22 +27,21 @@ def index(request):
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
-
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return render(request, 'accounts/login.html')
+            return redirect(request.GET.get('next') or 'accounts:index')
     else:
         form = AuthenticationForm()
     context = {
         'form': form
     }
-    return render(request, 'accounts/login.html', context)
+    return redirect('articles:index')
 
 # 로그아웃 페이지
 def logout(request):
     auth_logout(request)
-    return redirect('accounts:login')
+    return redirect('articles:index')
 
 
 # 개인 프로필 페이지
@@ -49,19 +49,20 @@ def detail(request,pk):
   user = get_user_model().objects.get(pk = pk)
   context = {
     'user': user,
+    'followers': user.followers.all(), 
+    'followings': user.followings.all()
   }
+
   return render(request,'accounts/detail.html', context)
 
 # 회원가입
 def signup(request):
-
   if request.method == 'POST':
     sign_form = CustomUserModel(request.POST, request.FILES)
     if sign_form.is_valid():
       sign = sign_form.save()
       auth_login(request, user=sign)
       return redirect('accounts:index')
-  
   else:
     sign_form = CustomUserModel()
 
@@ -71,43 +72,84 @@ def signup(request):
   
   return render(request, 'accounts/signup.html', context)
 
+#프로필 수정
+@login_required
+def edit_profile(request,pk):
 
-  #프로필 수정
-def edit_profile(request):
+    user = get_user_model().objects.get(pk=pk)
 
-    if request.method == 'POST':
-        form = CustomUserChangeForm(request.POST ,instance=request.user)
+    if request.user == user:
 
-        if form.is_valid():
-            user = form.save()  
-            if request.FILES :
-              user.profile_image =request.FILES['image']
+      if request.method == 'POST':
+          form = CustomUserChangeForm(request.POST ,instance=request.user)
+          if form.is_valid():
+              user = form.save()  
+              user.profile_image = request.FILES['image']
               user.save()
+              return redirect('accounts:detail',user.pk)
+      else:
+          form = CustomUserChangeForm(instance=request.user)
 
-            return redirect('accounts:index')
+      context = {
+          'form': form,
+      }
+
+      return render(request,'accounts/edit_profile.html',context)
     else:
-        form = CustomUserChangeForm(instance=request.user)
-    context = {
+      return redirect('accounts:wrong_approach')
+
+@login_required
+def change_password(request,pk):
+
+    user = get_user_model().objects.get(pk=pk)
+
+    if request.user == user:
+
+      if request.method == 'POST':
+          form = PasswordChangeForm(request.user, request.POST)
+          if form.is_valid():
+              user = form.save()
+              update_session_auth_hash(request, user)  # Important!
+              messages.success(request, 'Your password was successfully updated!')
+              return redirect('index')
+          else:
+              messages.error(request, 'Please correct the error below.')
+      else:
+          form = PasswordChangeForm(request.user)
+
+      context = {
         'form': form,
-    }
+      }
 
-    return render(request,'accounts/edit_profile.html',context)
-
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('accounts:index')
-        else:
-            messages.error(request, 'Please correct the error below.')
+      return render(request, 'accounts/change_password.html',context)    
     else:
-        form = PasswordChangeForm(request.user)
+      return redirect('accounts:wrong_approach')
+      #에러 페이지 -> index 수정
 
-    context = {
-      'form': form,
-    }
 
-    return render(request, 'accounts/change_password.html',context)
+# follow
+@login_required
+def follow(request, pk):
+  user = get_user_model().objects.get(pk=pk)
+
+  if request.user != user:
+    if request.user not in user.followers.all():
+      user.followers.add(request.user)
+      is_following = True
+    else:
+      user.followers.remove(request.user)
+      is_following = False
+
+  data = {
+    'isFollowing': is_following,
+    'followers': user.followers.all().count(),
+    'followings': user.followings.all().count(),
+  }
+
+  return JsonResponse(data)
+
+
+
+def wrong_approach(request):
+
+  return render(request,'accounts/wrong_approach.html')
