6f9e7f21d5016e6f330cddd45eb3c6c29b3b1da0
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-11-07 03:34:37 +0900

    본인좌표기준 관광지찍기

--- articles/urls.py
+++ articles/urls.py
@@ -11,10 +11,11 @@ urlpatterns = [
     path("<int:pk>/update/", views.update, name="review_update"),
     path("search/", views.search, name="search"),
     path("searchfail/", views.searchfail, name="searchfail"),
-    path("maps/", views.maps, name="maps"),
     path("<int:pk>/comment_create/", views.comment_create, name="comment_create"),
     path("<int:review_pk>/comment_delete/<int:comment_pk>/delete", views.comment_delete, name="comment_delete"),
     path("<int:review_pk>/comment_update/<int:comment_pk>/update", views.comment_update, name="comment_update"),
     path("<int:pk>/like/", views.like, name='like'),
     path("list/", views.list, name="list"),
+    path("maps2/", views.maps2, name="maps2"),
+    path("maps/<str:y>/<str:x>/", views.maps, name="maps"),
 ]
